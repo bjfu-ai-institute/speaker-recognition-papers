52c6b615895693956817a1eb7684999eb75502b7
vzxxbacq <fhq981014@gmail.com>
2018-04-06 14:35:15 +0800

    Update Ctdnn model

--- CT-DNN/models.py
+++ CT-DNN/models.py
@@ -1,21 +1,5 @@
 import tensorflow as tf
-import time
-import os
 import numpy as np
-import functools
-
-
-def lazy_property(function):
-    attribute = '_cache_' + function.__name__
-
-    @property
-    @functools.wraps(function)
-    def decorator(self):
-        if not hasattr(self, attribute):
-            setattr(self, attribute, function(self))
-        return getattr(self, attribute)
-
-    return decorator
 
 
 class DataManage(object):
@@ -30,6 +14,7 @@ class DataManage(object):
         self.enrollment_frames = np.array(enrollment_frames, dtype=np.float32)
         self.enrollment_targets = np.array(enrollment_targets, dtype=np.float32)
         self.batch_counter = 0
+        self.enroll_vector = []
         self.spkr_num = np.array(self.raw_labels).shape[-1]
 
     def next_batch(self):
@@ -50,51 +35,68 @@ class DataManage(object):
         return self.enrollment_frames, self.enrollment_targets, self.raw_frames, self.raw_labels
 
 
-class Model(object):
-    def __init__(self, n_gpu, max_step, sess, n_speaker,
-                 save_path='/home/user1/fhq/ctdnn/save',
-                 result_path='/home/user1/fhq/ctdnn/save'):
+class Model:
+    def __init__(self,
+                 n_gpu,
+                 n_speaker,
+                 model_name):
 
-        self.sess = sess
         self.n_speaker = n_speaker
-        self.result_path = result_path
-        self.save_path = save_path
-        self.max_step = max_step
-        self.frames = tf.placeholder(tf.float32, [None, 9, 40, 1], name='x')
-        self.target = tf.placeholder(tf.float32, [None, n_speaker], name='y')
+        self.name = model_name
+        self.batch_frames = tf.constant(value=0.0, shape=[65, 9, 40, 1], dtype=tf.float32)
+        self.batch_target = tf.constant(value=0.0, shape=[65, n_speaker], dtype=tf.float32)
         self.n_gpu = n_gpu
-        self.prediction
-        initializer = tf.global_variables_initializer()
-        sess.run(initializer)
+        self._prediction = None
+        vector = np.zeros(shape=[n_speaker, 400])
+        self.vectors = tf.Variable(vector, trainable=False, dtype=tf.float32)
+        self._loss = None
 
-    @lazy_property
+        with tf.variable_scope(model_name):
+            self.build_graph()
+
+    @property
+    def loss(self):
+        return self._loss
+
+    @property
     def prediction(self):
+        return self._prediction
 
-        conv1 = self.conv2d(self.frames, 'conv1', [3, 11, 1, 128], [1, 1, 1, 1], 'SAME')
+    @property
+    def feature(self):
+        return self._feature
 
-        pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 3, 3, 1], padding='VALID')
+    def build_graph(self):
+        """
+        Build the compute graph.
+        """
 
-        conv2 = self.conv2d(pool1, 'conv2', [3, 11, 128, 128], [1, 1, 1, 1], 'SAME')
+        # Inference
+        conv1 = self.conv2d(self.batch_frames, 'conv1', [4, 8, 1, 128], [1, 1, 1, 1], 'VALID')
 
-        pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 3, 3, 1], padding='VALID')
+        pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 3, 1], strides=[1, 2, 3, 1], padding='VALID')
+
+        conv2 = self.conv2d(pool1, 'conv2', [2, 4, 128, 128], [1, 1, 1, 1], 'VALID')
+
+        pool2 = tf.nn.max_pool(conv2, ksize=[1, 1, 2, 1], strides=[1, 1, 2, 1], padding='VALID')
 
         pool2_flat = tf.reshape(pool2, [-1,
-                                        pool2.get_shape().as_list()[1]*
-                                        pool2.get_shape().as_list()[2]*
+                                        pool2.get_shape().as_list()[1] *
+                                        pool2.get_shape().as_list()[2] *
                                         pool2.get_shape().as_list()[3]])
 
         bottleneck = self.full_connect(pool2_flat, 'bottleneck', 512)
 
         bottleneck_t = tf.reshape(bottleneck, [-1, 512, 1])
 
-        td1 = self.t_dnn(bottleneck_t, name='td1', shape=[2, 1, 128], strides=1)
-        td1_flat = tf.reshape(td1, [-1, td1.get_shape().as_list()[1]*td1.get_shape().as_list()[2]])
+        td1 = self.t_dnn(bottleneck_t, name='td1', shape=[5, 1, 128], strides=1)
+        td1_flat = tf.reshape(td1, [-1, td1.get_shape().as_list()[1] * td1.get_shape().as_list()[2]])
 
         p_norm1 = self.full_connect(td1_flat, name='P_norm1', units=2000)
         p_norm1_t = tf.reshape(p_norm1, [-1, 2000, 1])
 
-        td2 = self.t_dnn(p_norm1_t, name='td2', shape=[2, 1, 128], strides=1)
-        td2_flat = tf.reshape(td2, [-1, td2.get_shape().as_list()[1]*td2.get_shape().as_list()[2]])
+        td2 = self.t_dnn(p_norm1_t, name='td2', shape=[9, 1, 128], strides=1)
+        td2_flat = tf.reshape(td2, [-1, td2.get_shape().as_list()[1] * td2.get_shape().as_list()[2]])
 
         p_norm2 = self.full_connect(td2_flat, name='P_norm2', units=400)
 
@@ -102,116 +104,27 @@ class Model(object):
 
         output = self.full_connect(feature_layer, name='output', units=self.n_speaker)
 
-        return tf.nn.softmax(output), feature_layer
+        self._prediction = tf.nn.softmax(output)
 
-    @lazy_property
-    def loss(self):
-        logits = tf.placeholder(tf.float32, [None, self.n_speaker], name='logits')
-        feature = tf.placeholder(tf.float32, [None, 400], name='feature')
-        print("logits:", logits)
-        true_target = self.target
-        pred_id = tf.argmax(logits, axis=1)
-        pred_id = tf.reshape(pred_id, [-1, 1])
-        true_id = tf.argmax(true_target, axis=1)
-        true_id = tf.reshape(true_id, [-1, 1])
-        vector = self.calc_vector(feature, true_id)
-        loss = []
-        for vec_pred, vec_true in zip(vector[pred_id], vector[true_id]):
-            exp_all = 0
-            for index in range(self.n_speaker):
-                exp_all += tf.exp(self.cos_similar(vector[index], vec_pred))
-            exp_pred = tf.exp(self.cos_similar(vec_pred, vec_true))
-            loss.append(-exp_pred/exp_all)
-        loss = np.sum(np.array(loss))
-        return loss
+        # Update vectors
+        pred_index = tf.argmax(output, axis=1)
+        true_index = tf.argmax(self.batch_target, axis=1)
+        for i in range(self.n_speaker):
+            vec_index = tf.where(tf.equal(true_index, i))
+            vector = tf.reduce_mean(tf.gather_nd(feature_layer, vec_index), axis=0)
+            self.vectors[i].assign(vector)
 
-    @staticmethod
-    def train(sess, model, train_data, lr):
-        grads = []
-        for i in range(model.n_gpu):
-            frames, ids = train_data.next_batch()
-            with tf.device("/cpu:0"):
-                print('predict')
-                output, feature = sess.run(model.prediction, feed_dict={'x:0': frames})
-                print("output:", output)
-            with tf.device("/gpu:%d" % (i+3)):
-                grad = sess.run(tf.gradients(model.loss, tf.trainable_variables()),
-                                feed_dict={'logits:0': output, 'feature:0': feature, 'y:0': ids})
-                print('loss')
-
-                grads.append(grad)
-        with tf.device("/cpu:0"):
-            average_gradient = model.average_gradients(grads)
-            opt = tf.train.AdamOptimizer(lr)
-            train_op = opt.apply_gradients(average_gradient)
-        return train_op, np.sum(grads)
-
-    @staticmethod
-    def run(train_data, predict_data, lr, n_gpu, max_step):
-        assert type(train_data) == DataManage
-        assert type(predict_data) == DataManage
-        print("Enter succeed")
-        with tf.Graph().as_default():
-            with tf.Session(config=tf.ConfigProto(
-                allow_soft_placement=True,
-                log_device_placement=True,
-            )) as sess:
-                model = Model(n_speaker=train_data.spkr_num, sess=sess, n_gpu=n_gpu, max_step=max_step)
-                print("Init succeed")
-                start_time = time.time()
-                for step in range(model.max_step):
-                    _, loss = sess.run(Model.train(sess, model, train_data, lr))
-                    print("Running....")
-                    saver = tf.train.Saver()
-                    if not step % 10:
-                        string = ("%d steps in %d sec. loss= %.2f" % step, time.time()-start_time, loss)
-                        print(string)
-                    if step % 1000 == 0 or step == model.max_step-1:
-                        saver.save(sess, model.save_path)
-                enroll_frames, enroll_target, test_frames, test_target = predict_data.pred_data
-                _, feature = sess.run(model.prediction,
-                                      feed_dict={'x:0': enroll_frames})
-                true_id = np.argmax(enroll_target, axis=1)
-                true_id = np.reshape(true_id, [-1, 1])
-                vector = model.calc_vector(feature, true_id)
-                _, feature = sess.run(model.prediction, feed_dict={'x:0': test_frames})
-                predict_list = []
-                for i in feature:
-                    result = []
-                    for vec in vector:
-                        result.append(Model.cos_similar(vec, i))
-                    if result[np.argmax(result)[0]] > 0.6:
-                        predict_list.append(np.argmax(result))
-                    else:
-                        predict_list.append(model.n_speaker)
-                support = 0
-                false_reject = 0
-                false_accept = 0
-                for i in range(len(feature)):
-                    if predict_list[i] == y[i]:
-                        support += 1
-                    else:
-                        if predict_list[i] == spkr_num and y[i] != spkr_num:
-                            false_reject += 1
-                        elif predict_list[i] != spkr_num and y[i] == spkr_num:
-                            false_accept += 1
-
-                with open(os.path.join(model.result_path, 'result.txt')) as file:
-                    file.write(("ACC= %d, EER= %d" % (support/len(feature), false_reject/false_accept)))
-
-    @staticmethod
-    def average_gradients(grads):  # grads:[[grad0, grad1,..], [grad0,grad1,..]..]
-        averaged_grads = []
-        for grads_per_var in zip(*grads):
-            grads = []
-            for grad in grads_per_var:
-                expanded_grad = tf.expand_dims(grad, 0)
-                grads.append(expanded_grad)
-            grads = tf.concat(grads, 0)
-            grads = tf.reduce_mean(grads, 0)
-            averaged_grads.append(grads)
-
-        return averaged_grads
+        # Compute loss
+        loss = tf.Variable(0, dtype=tf.float32)
+        for i in range(pred_index.get_shape().as_list()[0]):
+            vec_ind = tf.gather_nd(pred_index, [i])
+            pred_vector = tf.gather_nd(self.vectors, [vec_ind])
+            vec_ind = tf.gather_nd(true_index, [i])
+            true_vector = tf.gather_nd(self.vectors, [vec_ind])
+            distance_1 = self.compute_exp_cosine(pred_vector, true_vector)
+            distance_2 = self.compute_exp_cosine(pred_vector)
+            loss = tf.add(tf.negative(tf.log(tf.divide(distance_1, distance_2))), loss)
+        self._loss = loss
 
     def t_dnn(self, x, shape, strides, name):
         with tf.name_scope(name):
@@ -242,18 +155,78 @@ class Model(object):
         return tf.Variable(initial, name=name)
 
     @staticmethod
-    def cos_similar(in_a, in_b):
-        in_a = np.mat(in_a)
-        in_b = np.mat(in_b)
-        num = float(in_a * in_b.T)
-        denom = np.linalg.norm(in_a) * np.linalg.norm(in_b)
-        return 0.5 + 0.5 * (num / denom)
+    def average_gradients(grads):  # grads:[[grad0, grad1,..], [grad0,grad1,..]..]
+        averaged_grads = []
+        for grads_per_var in zip(*grads):
+            grads = []
+            for grad in grads_per_var:
+                expanded_grad = tf.expand_dims(grad, 0)
+                grads.append(expanded_grad)
+            grads = tf.concat(grads, 0)
+            grads = tf.reduce_mean(grads, 0)
+            averaged_grads.append(grads)
+        return averaged_grads
 
-    @staticmethod
-    def calc_vector(feature, targets):
-        vectors = np.zeros([self.n_speaker, 400])
-        for index, target in zip(range(targets.get_shape().as_list()[0]), targets):
-            if vectors[target] == np.zeros(400):
-                vectors[target] = feature[index]
-            vectors[target] = np.add(vectors[index], feature[target])/2
-        return vectors
+    def compute_exp_cosine(self, vector1, vector2=None):
+        exp_sum = tf.Variable(0, trainable=False, dtype=np.float32)
+        vector1 = tf.expand_dims(vector1, dim=1)
+        if vector2 is not None:
+            vector2 = tf.expand_dims(vector2, dim=1)
+            x_norm = tf.sqrt(tf.reduce_sum(tf.square(vector1), axis=1))
+            y_norm = tf.sqrt(tf.reduce_sum(tf.square(vector2), axis=1))
+            x_y = tf.reduce_sum(tf.multiply(vector1, vector2), axis=1)
+            cos_similarity = tf.divide(x_y, tf.multiply(x_norm, y_norm))
+            tf.exp(tf.add(exp_sum, cos_similarity))
+            return exp_sum
+        else:
+            for i in range(self.n_speaker):
+                vector = tf.gather_nd(self.vectors, [i])
+                vector = tf.expand_dims(vector, dim=1)
+                x_norm = tf.sqrt(tf.reduce_sum(tf.square(vector1), axis=1))
+                y_norm = tf.sqrt(tf.reduce_sum(tf.square(vector), axis=1))
+                x_y = tf.reduce_sum(tf.multiply(vector1, vector), axis=1)
+                cos_similarity = tf.divide(x_y, tf.multiply(x_norm, y_norm))
+                tf.add(exp_sum, tf.exp(cos_similarity))
+            return exp_sum
+
+    def train_step(self, train_data, lr):
+        assert type(train_data) == DataManage
+        grads = []
+        opt = tf.train.AdamOptimizer(lr)
+        for i in range(self.n_gpu):
+            with tf.device("/gpu:%d" % i):
+                frames, targets = train_data.next_batch()
+                frames = tf.constant(frames, dtype=tf.float32)
+                targets = tf.constant(targets, dtype=tf.float32)
+                self.batch_frames.assign(frames)
+                self.batch_target.assign(targets)
+                gradient_all = opt.compute_gradients(self.loss)
+                grads.append(gradient_all)
+        with tf.device("/cpu:0"):
+            ave_grads = self.average_gradients(grads)
+            train_op = opt.apply_gradients(ave_grads)
+        return train_op, tf.reduce_sum(grads)
+
+
+def run(train_frames,
+        train_targets,
+        batch_size,
+        max_step,
+        save_path,
+        n_gpu,
+        lr):
+    with tf.Graph().as_default():
+        with tf.Session(config=tf.ConfigProto(
+                allow_soft_placement=True,
+                log_device_placement=True,
+        )) as sess:
+            train_data = DataManage(train_frames, train_targets, batch_size)
+            model = Model(n_gpu=n_gpu, model_name='ctdnn', n_speaker=train_data.spkr_num)
+            initial = tf.global_variables_initializer()
+            sess.run(initial)
+            saver = tf.train.Saver()
+            for i in range(max_step):
+                _, loss = sess.run(model.train_step(train_data, lr))
+                print(i, " loss:", loss)
+                if i % 25 == 0 or i + 1 == max_step:
+                    saver.save(sess, save_path)
