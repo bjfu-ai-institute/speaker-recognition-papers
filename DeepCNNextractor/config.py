744158f3ce8f55b9d9422b790b57ff31012a8887
Fang Huangqiang <fhq981014@gmail.com>
2018-06-10 10:55:29 +0800

    add dnn-ext model

---
+++ DeepCNNextractor/config.py
@@ -0,0 +1,21 @@
+"""
+Setting
+"""
+
+IS_BIG_DATASET = True
+
+URL_OF_BIG_DATASET = '/opt/fhq/data/ctdnn'
+
+LR = 1e4
+
+N_GPU = 4
+
+SAVE_PATH = '/home/fang-hq/Documents/result/ctdnn'
+
+MAX_STEP = 75
+
+BATCH_SIZE = 100
+
+N_SPEAKER = 166
+
+MODEL_NAME = 'deep_cnn_feature_ext'
